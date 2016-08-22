ed3b87d0d62b19f404c0222e2c4696a5d9ebccb9
michaeltrilford <trilfordm@gmail.com>
2016-08-22 21:58:40 +1000

    10k update

--- js/main.js
+++ js/main.js
@@ -1,10 +1,86 @@
-var $=function(id){return document.getElementById(id)};var a=document.createElement('a');$('author-info').appendChild(a).id="toggle";var span1=document.createElement('span');var span2=document.createElement('span');$('toggle').appendChild(span1).id="f-c";$('toggle').appendChild(span2).id="l-c";$('f-c').innerHTML='More info';$('l-c').innerHTML='Less info';$('l-c').className="h";var objectHeight=document.getElementById('a-c').clientHeight;$('a-c').className="fadeOut";$('a-c').style.maxHeight=0;$('f-c').onclick=function(){$('a-c').className="fadeOut fadeIn";$('a-c').style.maxHeight=objectHeight+"px";$('f-c').className="h";$('l-c').className="s"}
-$('l-c').onclick=function(){$('a-c').className="fadeOut";$('a-c').style.maxHeight=0;$('f-c').className="s";$('l-c').className="h"}
-var scrollHeader=document.getElementsByClassName('author');scrollHeader.id="scrollHeader";var title=document.createElement('strong');var rule=document.createElement('hr');$('scrollHeader').appendChild(title).id="title";$('title').innerHTML='Tips for Your First 10K';$('title').style.display="none";$('title').className="nmb";var scrollpos=window.scrollY;var scrollHeaderEl=document.getElementById("scrollHeader");function add_class_on_scroll(){$('scrollHeader').className="cf author scrolled";$('title').style.display="block"}
-function remove_class_on_scroll(){$('scrollHeader').className="cf author";$('title').style.display="none"}
-window.addEventListener('scroll',function(){scrollpos=window.scrollY;if(scrollpos>65){add_class_on_scroll()}
-else{remove_class_on_scroll()}
-console.log(scrollpos)})
+// KEY
+
+// m-i = More info
+// a-c = Author Content
+// l-i = Less info
+// sH = scrollHeader
+// t = title
+// tg = Toggle
+// fO = FadeOut
+// fI = FadeIn
+// a = Author
+
+// ID Selector
+var $ = function( id ) { return document.getElementById( id ); };
+
+  // Create and append button
+  var a = document.createElement('a');
+  // Add <a> and add toogle id
+  $('author-info').appendChild(a).id = "tg";
+
+  // Create content for toggle
+  var span1 = document.createElement('span');
+  var span2 = document.createElement('span');
+  	// Add Id
+  	$('tg').appendChild(span1).id = "m-i";
+  	$('tg').appendChild(span2).id = "l-i";
+  	// Add span content
+  	$('m-i').innerHTML = 'More info';
+  	$('l-i').innerHTML = 'Less info';
+  	$('l-i').className = "h";
+
+  // Get original height of element to hide
+  var objectHeight = document.getElementById('a-c').clientHeight;
+
+  // Hide Author Content
+  // Add fadeOut
+  $('a-c').className = "fO";
+  $('a-c').style.maxHeight=0;
+  // Reveal Author Content 
+  $('m-i').onclick = function() {
+    $('a-c').className = "fO fI";
+    $('a-c').style.maxHeight= objectHeight+"px";
+    $('m-i').className = "h";
+    $('l-i').className = "s";
+  }
+  // Hide Author Content 
+  $('l-i').onclick = function() {
+    $('a-c').className = "fO";
+    $('a-c').style.maxHeight=0;
+    $('m-i').className = "s";
+    $('l-i').className = "h";
+  }
+
+  // Create Scroll Header
+  var sH = document.getElementsByClassName('a');
+  sH.id = "sH";
+  var title = document.createElement('strong');
+  var rule = document.createElement('hr');
+  $('sH').appendChild(title).id = "t";
+  $('t').innerHTML = 'Tips for Your First 10K';
+  $('t').className = "nmb";
+
+
+  // On Scroll Header
+  function add_class_on_scroll() {
+  		$('sH').className = "cf nmb a scrolled";
+  }
+  function remove_class_on_scroll() {
+  		$('sH').className = "cf nmb a";
+  }
+  // Get original height of element to hide
+  var hHeight = document.getElementById('header').clientHeight;
+  window.addEventListener('scroll', function(){ 
+      scrollpos = window.scrollY;
+      if(scrollpos > hHeight){
+          add_class_on_scroll();
+      }
+      else {
+          remove_class_on_scroll();
+      }
+      console.log(scrollpos);
+  });
+
 
 
 
