77a906dcc0d5b79061fdf0ad6fd69f3853f88948
Mike Trilford <trilfordm@gmail.com>
2025-05-24 19:09:19 +1000

    Updated the rule component

--- mui-rule/index.js
+++ mui-rule/index.js
@@ -1,30 +1,30 @@
 class muiRule extends HTMLElement {
   static get observedAttributes() {
-    return ["length", "weight", "direction"];
+    return ['length', 'weight', 'direction'];
   }
 
   constructor() {
     super();
-    const shadowRoot = this.attachShadow({ mode: "open" });
+    const shadowRoot = this.attachShadow({ mode: 'open' });
     let direction = `horizontal`;
     let length = `100%`;
-    let weight = "1px";
-    this.setAttribute("role", "presentation");
-    this.setAttribute("direction", this.getAttribute("direction") || direction);
-    this.setAttribute("length", this.getAttribute("length") || length);
-    this.setAttribute("weight", this.getAttribute("weight") || weight);
+    let weight = '1px';
+    this.setAttribute('role', 'presentation');
+    this.setAttribute('direction', this.getAttribute('direction') || direction);
+    this.setAttribute('length', this.getAttribute('length') || length);
+    this.setAttribute('weight', this.getAttribute('weight') || weight);
     const styles = `
       :host {
         display: block;
-        background: var(--grey-200);
+        background: var(--border-color);
       }
       :host([direction="horizontal"]) {
-        width: ${this.getAttribute("length") || length};
-        height: ${this.getAttribute("weight") || weight};
+        width: ${this.getAttribute('length') || length};
+        height: ${this.getAttribute('weight') || weight};
       }
       :host([direction="vertical"]) {
-        height: ${this.getAttribute("length") || length};
-        width: ${this.getAttribute("weight") || weight};
+        height: ${this.getAttribute('length') || length};
+        width: ${this.getAttribute('weight') || weight};
       }
     `;
     shadowRoot.innerHTML = `
@@ -33,4 +33,4 @@ class muiRule extends HTMLElement {
   }
 }
 
-customElements.define("mui-rule", muiRule);
+customElements.define('mui-rule', muiRule);
